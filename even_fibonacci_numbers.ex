b7d024ca8ba63f6f00743a6c65bee09d4a507c47
yuuki murakami <ymurakami@221x112x40x66.ap221.ftth.ucom.ne.jp>
2017-11-08 15:13:35 +0900

    add only even number

--- even_fibonacci_numbers.ex
+++ even_fibonacci_numbers.ex
@@ -1,12 +1,19 @@
 defmodule EvenFibonacchiNumbers do
+  import Integer
+  
+  @maxnumber 4000000
+
   def run do
-    sum = _run(0, 1, 0, [true, true])
-    IO.puts sum
+    _run(0, 1, 0, [true, false])
   end
 
-  defp _run(n, m, sum, [false,_]), do: sum
-  defp _run(n, m, sum, [true,_]) do
-    _run(m, n+m, sum+n+m, [4000000 > n+m,true])
+  defp _run(_, _, sum, [false, _]), do: sum
+  defp _run(n, m, sum, [true, false]) do
+    fib = n+m
+    _run(m, fib, sum, [@maxnumber > fib, is_even(fib+m)])
   end
-
+  defp _run(n, m, sum, [true, _]) do
+    fib = n+m
+    _run(m, fib, sum+fib, [@maxnumber > fib, is_even(fib+m)])
+  end
 end
