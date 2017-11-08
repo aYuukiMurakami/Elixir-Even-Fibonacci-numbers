80df6921072b6dca63f33b7fe387b78ce281ba34
yuuki murakami <ymurakami@221x112x40x66.ap221.ftth.ucom.ne.jp>
2017-11-08 14:52:24 +0900

    "if" not use

--- even_fibonacci_numbers.ex
+++ even_fibonacci_numbers.ex
@@ -1,15 +1,12 @@
 defmodule EvenFibonacchiNumbers do
   def run do
-    sum = _run(0, 1, 0)
+    sum = _run(0, 1, 0, [true, true])
     IO.puts sum
   end
 
-  defp _run(n, m, sum) do
-    if 4000000 > n+m do
-      _run(m, n+m, sum+n+m)
-    else
-      sum
-    end
+  defp _run(n, m, sum, [false,_]), do: sum
+  defp _run(n, m, sum, [true,_]) do
+    _run(m, n+m, sum+n+m, [4000000 > n+m,true])
   end
 
 end
