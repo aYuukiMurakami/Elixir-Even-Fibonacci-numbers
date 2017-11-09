04b123e50bc4eaf5793d4b083e70a534212d2083
yuuki murakami <ymurakami@221x112x40x66.ap221.ftth.ucom.ne.jp>
2017-11-09 11:03:49 +0900

    delete default test

--- test/even_fibonacci_numbers_test.exs
+++ test/even_fibonacci_numbers_test.exs
@@ -1,8 +1,4 @@
 defmodule EvenFibonacciNumbersTest do
   use ExUnit.Case
   doctest EvenFibonacciNumbers
-
-  test "greets the world" do
-    assert EvenFibonacciNumbers.hello() == :world
-  end
 end
